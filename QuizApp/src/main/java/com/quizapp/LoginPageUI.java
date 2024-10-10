61a305e01e985065c63e8c0bfa403f9f4c7deaad
MD. Yasir Arafat <md.yasir.arafat.1.1@gmail.com>
2024-10-10 23:41:45 +0600

    Update LoginPageUI.java

--- QuizApp/src/main/java/com/quizapp/LoginPageUI.java
+++ QuizApp/src/main/java/com/quizapp/LoginPageUI.java
@@ -1,6 +1,6 @@
 package com.quizapp;
 
-import com.quizapp.quizapp.LoginActions;
+import com.quizapp.LoginActions;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
